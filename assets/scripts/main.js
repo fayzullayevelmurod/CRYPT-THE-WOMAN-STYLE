8ed67a6e9a94c52249a0b1e33e97bc197ed3fe55
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2026-03-19 10:25:16 +0500

    slider section the end

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,23 +1,21 @@
 
-let swiper = new Swiper(".treatmentsSwiper", {
-  slidesPerView: 1.10,
-  spaceBetween: 28,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+let swiper = new Swiper(".sliderSwiper", {
+  spaceBetween: 50,
+  slidesPerView: 2,
+  centeredSlides: true,
+  initialSlide: 1,
   breakpoints: {
     640: {
-      slidesPerView: 2.2,
-      spaceBetween: 28,
+      slidesPerView: 1.7,
+      spaceBetween: 60,
     },
     993: {
       slidesPerView: 3.2,
-      spaceBetween: 28,
+      spaceBetween: 100,
     },
-    1200: {
-      slidesPerView: 3.6,
-      spaceBetween: 40,
+    1400: {
+      slidesPerView: 2.4,
+      spaceBetween: 392,
     },
   },
 });
